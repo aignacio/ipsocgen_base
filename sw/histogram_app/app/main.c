8c3e5c9bba70525ee16bf738caec240c12affe15
Anderson Ignacio <anderson@aignacio.com>
2023-06-11 17:21:25 +0100

    Start changing the eth appi
    Signed-off-by: Anderson Ignacio <anderson@aignacio.com>

--- sw/histogram_app/app/main.c
+++ sw/histogram_app/app/main.c
@@ -15,86 +15,114 @@
 #include "irq_ctrl.h"
 #include "dma.h"
 #include "eth.h"
+#include "soc_types.h"
+
+TaskHandle_t xHandleProcCmd;
+TaskHandle_t xHandleProcImg;
+
+static SemaphoreHandle_t xDMAMutex;
+static SemaphoreHandle_t xHistogramMutex;
+
+static QueueHandle_t xStartFetchImgQ;
+static QueueHandle_t xDataReqQ;
+static QueueHandle_t xEthSentQ;
+static QueueHandle_t xDMADoneQ;
 
 int main( void );
 
-static void print_task_1(void *pvParameters){
-  uint8_t test = 0;
-  while(1){
-    dbg("\nTask - 1 / Test: %d",test);
-    test += 2;
-    vTaskDelay(pdMS_TO_TICKS(100));
+static void vprvProcCmd (void *pvParameters) {
+  for (;;) {
   }
 }
 
-static void print_task_2(void *pvParameters){
-  uint8_t test = 0;
-  while(1){
-    dbg("\nTask - 2 / Test: %d",test);
-    test += 4;
-    vTaskDelay(pdMS_TO_TICKS(200));
+static void vprvProcImg (void *pvParameters) {
+  for (;;) {
   }
 }
 
-static void print_task_3(void *pvParameters){
-  uint8_t test = 0;
-  while(1){
-    dbg("\nTask - 3 / Test: %d",test);
-    test += 10;
-    vTaskDelay(pdMS_TO_TICKS(300));
-  }
-}
 
-static void print_task_4(void *pvParameters){
-  uint8_t test = 0;
-  while(1){
-    dbg("\nTask - 4 / Test: %d",test);
-    test += 10;
-    vTaskDelay(pdMS_TO_TICKS(600));
-  }
+static void vprvSetEth (void) {
+  EthLocalCfg_t xLocalCfg = {
+    .IPAddr.bytes     = {192, 168,   1, 130},
+    .IPGateway.bytes  = {192, 168,   1,   1},
+    .SubnetMask.bytes = {255, 255, 255,   0},
+    .MACAddr.bytes    = {0x00, 0x00, 0x00, 0x0A, 0x35, 0xA2, 0x34, 0x56} // Xilinx Inc.
+  };
+
+  EthCfg_t xSendCfg = {
+    .Src              = 1234,
+    .Dst              = 1234,
+    .Len              = 4,
+    .IPAddr.bytes     = {192, 168,   1, 223},
+    .MACAddr.bytes    = {0x00, 0x00, 0x04, 0x42, 0x1a, 0x09, 0xaf, 0xc7}
+  };
+
+  EthFilterCfg_t xFilterCfg = {
+    .Enable  = 1,
+    .UDPPort = 1234,
+    .IPAddr  = xSendCfg.IPAddr
+  };
+
+  vEthSetLocalCfg(xLocalCfg);
+  vEthSetSendCfg(xSendCfg);
+  vEthSetFilter(xFilterCfg);
+
+  dbg("Ethernet Configured");
 }
 
 int main(void){
-  BaseType_t xReturned;
-  TaskHandle_t xHandle1 = NULL,
-               xHandle2 = NULL,
-               xHandle3 = NULL,
-               xHandle4 = NULL;
+  BaseType_t xReturned = pdFALSE;
+
+  // Initialize the ETHERNET
+  vprvSetEth();
+
+  // IRQs
+  // Enable IRQs
+  vIRQClearMaskSingle(irqMASK_DMA_0_ERROR);
+  vIRQClearMaskSingle(irqMASK_DMA_0_DONE);
+  vIRQClearMaskSingle(irqMASK_ETH_RECV);
+  vIRQClearMaskSingle(irqMASK_ETH_SENT);
+  /*vIRQClearMaskSingle(irqMASK_ETH_RECV_FULL);*/
+
+  // DMA
+  vDMAInit();
+
+  // Initialize the mutex
+  xDMAMutex = xSemaphoreCreateMutex();
+  xHistogramMutex = xSemaphoreCreateMutex();
+
+  if ((xDMAMutex == NULL) || (xHistogramMutex == NULL)) {
+    masterCRASH_DBG_INFO("Cannot create the mutexes");
+  }
+
+  xDataReqQ = xQueueCreate(2, sizeof(uint32_t));
+  xDMADoneQ = xQueueCreate(1, sizeof(uint32_t));
+  xEthSentQ = xQueueCreate(1, sizeof(uint8_t));
+  xStartFetchImgQ = xQueueCreate(1, sizeof(CmdType_t));
 
   xReturned = xTaskCreate(
-    print_task_1,
-    "T1",
+    vprvProcCmd,
+    "Process Commands",
     configMINIMAL_STACK_SIZE*2U,
     NULL,
     tskIDLE_PRIORITY+1,
-    &xHandle1);
+    &xHandleProcCmd);
+  masterCHECK_TASK(xReturned);
 
   xReturned = xTaskCreate(
-    print_task_2,
-    "I2",
+    vprvProcImg,
+    "Process Images",
     configMINIMAL_STACK_SIZE*2U,
     NULL,
     tskIDLE_PRIORITY+1,
-    &xHandle2);
-
-  xReturned = xTaskCreate(
-    print_task_3,
-    "T3",
-    configMINIMAL_STACK_SIZE*2U,
-    NULL,
-    tskIDLE_PRIORITY+1,
-    &xHandle3);
-
-  xReturned = xTaskCreate(
-    print_task_4,
-    "T4",
-    configMINIMAL_STACK_SIZE*2U,
-    NULL,
-    tskIDLE_PRIORITY+1,
-    &xHandle4);
+    &xHandleProcImg);
+  masterCHECK_TASK(xReturned);
 
   vTaskStartScheduler();
+
+  // Should never reach here...
   for(;;);
+
 }
 
 void vSystemIrqHandler(uint32_t ulMcause){
